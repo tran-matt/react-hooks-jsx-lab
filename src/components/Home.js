878f7bff62ac3ecb24c40df8b3cd2bc17a4b142e
Matthew Tran <matt.h.tran@gmail.com>
2023-10-23 11:29:43 -0400

    complete lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,18 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
+
+  const h1Style = {
+          color: 'firebrick',
+    };
+
   // update the JSX being returned!
-  return <div>Home</div>;
-}
+  return (
+  <div id="home">
+    Home
+    <h1 style={h1Style}>{name} is a Web Developer from {city}</h1>
+    </div>
+  );
+};
 
 export default Home;
